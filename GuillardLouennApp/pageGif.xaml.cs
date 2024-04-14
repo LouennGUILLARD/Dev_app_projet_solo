3e226b2b250eedbbb15360c3c92492841fcc02aa
Louenn GUILLARD <louennguillard.pro@gmail.com>
2024-04-14 21:17:51 +0200

    modification image et ajout gif

---
+++ GuillardLouennApp/pageGif.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuillardLouennApp;
+
+public partial class pageGif : ContentPage
+{
+    public pageGif()
+    {
+        InitializeComponent();
+    }
+
+    public async void Button_back(object sender, EventArgs e)
+    {
+        await Navigation.PopAsync();
+    }
+    
+    protected async override void OnAppearing()
+    {
+        base.OnAppearing();
+        await Task.Delay(100);
+        imgLoader.IsAnimationPlaying = true;
+    }
+    
+}
