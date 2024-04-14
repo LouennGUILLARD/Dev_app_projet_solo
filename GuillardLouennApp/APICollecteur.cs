1cf9db63ec41767572cc3c2ccbec6b06b4c91585
Louenn GUILLARD <louennguillard.pro@gmail.com>
2024-04-14 21:55:04 +0200

    ajout connexion et affichage des données brut de l'API

---
+++ GuillardLouennApp/APICollecteur.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GuillardLouennApp
+{
+    public class APICollecteur
+    {
+        private readonly HttpClient _client;
+
+        public APICollecteur()
+        {
+            _client = new HttpClient
+            {
+                BaseAddress = new Uri("https://api.sampleapis.com/switch/games")
+            };
+        }
+
+        public async Task<string> GetAsync(string endpoint)
+        {
+            var response = await _client.GetAsync(endpoint);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+        
+        public async Task AfficherDonneesAsync(string endpoint)
+        {
+            var donnees = await GetAsync(endpoint);
+            Console.WriteLine(donnees);
+        }
+        
+        
+    }
+}
--- GuillardLouennApp/MainPage.xaml.cs
+++ GuillardLouennApp/MainPage.xaml.cs
@@ -3,9 +3,12 @@
     public partial class MainPage : ContentPage
     {
 
+        private readonly APICollecteur _api;
+        
         public MainPage()
         {
             InitializeComponent();
+            _api = new APICollecteur();
         }
 
         private void GoToGifPage(object sender, EventArgs e)
@@ -16,6 +19,12 @@
         private void GoToVideoPage(object sender, EventArgs e)
         {
             Navigation.PushAsync(new pageVideo());
+        }
+        
+        private async void BtnAfficherDonnees_Clicked(object sender, EventArgs e)
+        {
+            var donnees = await _api.GetAsync(null);
+            lblDonnees.Text = donnees;
         }
     }
 
