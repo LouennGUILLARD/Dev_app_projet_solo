b1e92b7ea61c5b587cce080de0c2a05dbf2a8151
Louenn GUILLARD <louennguillard.pro@gmail.com>
2024-04-14 21:23:59 +0200

    ajout vidéo

---
+++ GuillardLouennApp/pageVideo.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuillardLouennApp;
+
+public partial class pageVideo : ContentPage
+{
+    public pageVideo()
+    {
+        InitializeComponent();
+    }
+    
+    public async void Button_back(object sender, EventArgs e)
+    {
+        await Navigation.PopAsync();
+    }
+}
